e73278968e01550d2ae8bb81ea7d98a734801164
ptya <petr.szab@gmail.com>
2019-11-20 19:42:47 +0100

    initials

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Peter Szabo',
+    titleTemplate: '%s · Web Development',
+    description: 'Peter Szabo, Web Developer',
+    url: 'https://www.zsuzsiespetiegybekel.hu',
+    image: '/images/favicon.ico', // TODO:
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +27,23 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, `src`, `pages`),
+        assets: path.join(__dirname, `src`, `assets`),
+        components: path.join(__dirname, `src`, `components`),
+        utils: path.join(__dirname, `src`, `utils`),
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `assets`, `images`),
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
