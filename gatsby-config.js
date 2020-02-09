1337abb395ae9660dd814ccf9cee926a99007afb
ptya <petr.szab@gmail.com>
2020-02-09 16:25:59 +0100

    finalized sharing image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Zsuzsi & Peti egybekel',
     description: 'Minden, amit tudni lehet Zsuzsi & Peti esküvőjéről :)',
     url: 'https://zsuzsiespetiegybekel.hu',
-    image: '/images/share_new.jpg',
+    image: '/images/share.jpg',
     author: 'Peter Szabo',
   },
   plugins: [
